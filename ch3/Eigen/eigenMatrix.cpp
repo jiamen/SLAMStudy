0d221fda72c9776388857438334baaefd60b4165
jiamen <2731275645@qq.com>
2021-07-09 21:00:24 +0800

    eigen

--- ch3/Eigen/eigenMatrix.cpp
+++ ch3/Eigen/eigenMatrix.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* *argv)
     // 输入数据（初始化）
     matrix_23 << 1, 2, 3, 4, 5, 6;
     // 输出
-    cout << matrix_23 << endl;
+    cout << "matrix_23 = " << endl << matrix_23 << endl;
 
     // 用()访问矩阵中的元素
     for (int i=0; i<2; i ++)
